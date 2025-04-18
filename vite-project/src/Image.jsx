4def4a433702688fbc0048c1febeb80c6fdb1163
Aditya Kumar <adityakr123muz@gmail.com>
2025-04-18 23:11:25 +0530

    add some changes

--- vite-project/src/Image.jsx
+++ vite-project/src/Image.jsx
@@ -4,7 +4,7 @@ function Image() {
   return (
    <>
     <div className="relative">
-        <img src="/5.png" 
+        <img src="/5.jpg" 
         alt="Banner" className="w-full h-130" />
         <div className="absolute top-1/4 left-10 text-white">
           <h1 className="text-4xl font-bold leading-tight"></h1>
